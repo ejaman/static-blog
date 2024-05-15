9d8cd71fd522f6d68f2eabf0b32c0ed2d6970885
ejaman <leegm17@naver.com>
2024-05-15 17:17:27 +0900

    feat: add getAllPosts function

---
+++ src/service/post.ts
@@ -0,0 +1,23 @@
+import { readFile } from "fs/promises";
+import path from "path";
+
+export type Category = "Ilsang" | "tips" | "next";
+
+export type Post = {
+  title: string;
+  description: string;
+  date: string;
+  category: Category;
+  path: string;
+  featured: boolean;
+};
+
+export async function getAllPosts(): Promise<Post[]> {
+  const filePath = path.join(process.cwd(), "data", "posts.json");
+  return (
+    readFile(filePath, "utf-8")
+      // .then<Post[]>((data) => JSON.parse(data))
+      .then<Post[]>(JSON.parse) // 위와 같은 코드
+      .then((posts) => posts.sort((a, b) => (a.date > b.date ? 1 : -1)))
+  );
+}
