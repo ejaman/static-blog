7e5d0cb286b78f31ad46b25b71d216df1a0384f9
ejaman <leegm17@naver.com>
2024-05-16 19:44:12 +0900

    feat: 공통 그리드 UI 구현

--- src/components/molecules/PostGrid.tsx
+++ src/components/molecules/PostGrid.tsx
@@ -5,10 +5,11 @@ import PostCard from "../atoms/PostCard";
 type Props = { posts: Post[] };
 export default function PostGrid({ posts }: Props) {
   return (
-    <ul>
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {posts.map((post) => (
-        // <li key={post.path}>{post.title}</li>
-        <PostCard key={post.path} post={post} />
+        <li key={post.path}>
+          <PostCard key={post.path} post={post} />
+        </li>
       ))}
     </ul>
   );
