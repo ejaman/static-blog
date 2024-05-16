9fdf4f6f66c4d2597df2bc846567e0a066558cd2
ejaman <leegm17@naver.com>
2024-05-16 20:37:22 +0900

    feat: modify padding

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default async function PostsPage() {
   const posts = await getFeaturedPosts();
   return (
-    <div className="flex flex-col gap-32 px-28 py-10">
+    <div className="flex flex-col gap-32 px-10 py-10">
       <div className="flex flex-col gap-10">
         <h2 className="text-xl font-bold my-2">Ilsang Posts</h2>
         <PostGrid posts={posts} />
