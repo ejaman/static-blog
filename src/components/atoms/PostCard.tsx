5c283fe1ab3a5cc6696e7764b1d7190de692b1fb
ejaman <leegm17@naver.com>
2024-05-16 19:43:37 +0900

    feat: post card UI 구현

--- src/components/atoms/PostCard.tsx
+++ src/components/atoms/PostCard.tsx
@@ -1,23 +1,29 @@
 import { Post } from "@/service/post";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type Props = { post: Post };
-export default function PostCard({ post }: Props) {
-  const { title, description, date, category, path, featured } = post;
+export default function PostCard({
+  post: { title, description, date, category, path, featured },
+}: Props) {
   return (
-    <li className="">
-      <div>
-        <div>
-          <Image src="" alt="card" />
+    <Link href={`/post/${path}`} className="">
+      <article className="rounded-sm overflow-hidden shadow-sm border border-gray">
+        <Image
+          className="w-full"
+          width={220}
+          height={150}
+          src={`/images/posts/${path}.png`}
+          alt={`${path}-card`}
+        />
+        <div className="flex flex-col p-4">
+          <div className="text-base font-semibold">{title}</div>
+          <p className="w-full truncate text-sm">{description}</p>
+          <time className="text-xs">{date.toString()}</time>
+          <div className="text-xs self-end font-semibold">{category}</div>
         </div>
-        <div>
-          <div>{title}</div>
-          <div>{description}</div>
-          <div>{date}</div>
-          <div>{category}</div>
-        </div>
-      </div>
-    </li>
+      </article>
+    </Link>
   );
 }
