96ad7cde630c3dc3f93ee2f3b1a42fed7ee40458
ejaman <leegm17@naver.com>
2024-05-16 20:34:45 +0900

    feat: posts layout 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,24 @@
+import PostCard from "@/components/atoms/PostCard";
+import PostGrid from "@/components/molecules/PostGrid";
+import { getFeaturedPosts } from "@/service/post";
 import React from "react";
 
-export default function PostsPage() {
-  return <div>Posts Page</div>;
+export default async function PostsPage() {
+  const posts = await getFeaturedPosts();
+  return (
+    <div className="flex flex-col gap-32 px-28 py-10">
+      <div className="flex flex-col gap-10">
+        <h2 className="text-xl font-bold my-2">Ilsang Posts</h2>
+        <PostGrid posts={posts} />
+      </div>
+      <div className="flex flex-col gap-10">
+        <h2 className="text-xl font-bold my-2">Tips Posts</h2>
+        <PostGrid posts={posts} />
+      </div>
+      <div className="flex flex-col gap-10">
+        <h2 className="text-xl font-bold my-2">Next Posts</h2>
+        <PostGrid posts={posts} />
+      </div>
+    </div>
+  );
 }
